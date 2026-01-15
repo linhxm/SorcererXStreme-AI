0bd45a8ba7f5f3828f57db6f99269d99025b824f
LinhXM <nguyenvanlinh.1710.it@gmail.com>
2026-01-15 18:50:22 +0700

    Remove Test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-try:
-    # Thử lấy ID của tài khoản đang kết nối
-    sts = boto3.client('sts')
-    print("Kết nối thành công!")
-    print(f"Account ID: {sts.get_caller_identity()['Account']}")
-except Exception as e:
-    print(f"Lỗi kết nối: {e}")
